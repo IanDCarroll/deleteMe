ba5cebf57f51c307da008f2357ab731634267a99
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-18 14:55:55 -0700

    run test 2

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,5 +1,21 @@
 require 'rspec/given'
 
 describe 'Acceptance Test' do
-
+  Given (:s) { StringScanner.new('This is an example string') }
+  Then{ false     == s.eos?        }
+  And { "This"    == s.scan(/\w+/) }
+  And { nil       == s.scan(/\w+/) }
+  And { " "       == s.scan(/\s+/) }
+  And { nil       == s.scan(/\s+/) }
+  And { "is"      == s.scan(/\w+/) }
+  And { false     == s.eos?        }
+  And { " "       == s.scan(/\s+/) }
+  And { "an"      == s.scan(/\w+/) }
+  And { " "       == s.scan(/\s+/) }
+  And { "example" == s.scan(/\w+/) }
+  And { " "       == s.scan(/\s+/) }
+  And { "string"  == s.scan(/\w+/) }
+  And { true      == s.eos?        }
+  And { nil       == s.scan(/\s+/) }
+  And { nil       == s.scan(/\w+/) }
 end
