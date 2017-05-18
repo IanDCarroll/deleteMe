3ca87b5cff6a756fbe886a3671401a40d5762bd0
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-18 15:20:40 -0700

    run test 6

--- spec/ian_strscan_spec.rb
+++ spec/ian_strscan_spec.rb
@@ -5,3 +5,11 @@ describe 'IanStrscan init' do
   Given(:subject) { IanStrscan.new("abc def") }
   Then { raise_error != subject }
 end
+
+describe 'IanStrscan eos?' do
+  context 'when string is not empty' do
+    Given(:string) { "abc def" }
+    When(:subject) { IanStrscan.new string }
+    Then { false == subject.eos? }
+  end
+end
