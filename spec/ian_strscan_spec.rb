0148f15260e41bd4eb1b3a0b2c536f0975f3b7fc
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-19 14:58:11 -0700

    add eos? method to IanStrscan

--- spec/ian_strscan_spec.rb
+++ spec/ian_strscan_spec.rb
@@ -5,3 +5,11 @@ describe 'IanStrscan init' do
   Given(:example) { IanStrscan.new('abc def') }
   Then { raise_error != example }
 end
+
+describe 'IanStrscan eos?'do
+  context 'when string is not empty' do
+    Given(:string) { "abc_def" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { false == subject.eos? }
+  end
+end
