06a2b7dd4cdf8e5dcd7ff7f7f415059c22377d9c
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-19 12:27:15 -0700

    fail .eos? returns true if end_of_string

--- spec/ian_strscan_spec.rb
+++ spec/ian_strscan_spec.rb
@@ -8,8 +8,14 @@ end
 
 describe 'IanStrscan eos?' do
   context 'when string is not empty' do
-    Given(:string) {"abc def"}
+    Given(:string) { "abc def" }
     When(:subject) { IanStrscan.new string }
     Then { false == subject.eos? }
+  end
+
+  context 'when string is empty' do
+    Given(:string) { "" }
+    When(:subject) { IanStrscan.new string }
+    Then { true == subject.eos? }
   end
 end
