9727e012bc6ab09ac0e73734434a14b05e762650
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-19 12:20:39 -0700

    fail .eos? returns false if not end-fo-string

--- spec/ian_strscan_spec.rb
+++ spec/ian_strscan_spec.rb
@@ -1,7 +1,15 @@
 require 'rspec/given'
 require 'ian_strscan'
 
-describe 'Ianstrscan init' do
+describe 'IanStrscan init' do
   Given(:subject) { IanStrscan.new('abc def') }
   Then { raise_error != subject }
+end
+
+describe 'IanStrscan eos?' do
+  context 'when string is not empty' do
+    Given(:string) {"abc def"}
+    When(:subject) { IanStrscan.new string }
+    Then { false == subject.eos? }
+  end
 end
