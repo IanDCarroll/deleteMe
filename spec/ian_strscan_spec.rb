7c59b8cbaac2fa657bffa08b5148919f71a5ca61
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-19 16:24:20 -0700

    Pass Acceptance; add scan method

--- spec/ian_strscan_spec.rb
+++ spec/ian_strscan_spec.rb
@@ -19,3 +19,38 @@ describe 'IanStrscan eos?' do
     Then { true == subject.eos? }
   end
 end
+
+describe 'IanStrscan scan' do
+  context 'when regex matches the current position' do
+    Given(:string) { "abc def" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { 'abc' == subject.scan(/\w+/) }
+  end
+
+  context 'when regex does not match' do
+    Given(:string) { "abc def" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { nil == subject.scan(/\s+/) }
+  end
+
+  context 'when scan remembers its current position' do
+    Given(:string) { "abc def" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { 'abc' == subject.scan(/\w+/) }
+    And  { ' ' == subject.scan(/\s+/) }
+    And  { 'def' == subject.scan(/\w+/) }
+ end
+
+  context 'when scan does not destroy the input string' do
+    Given(:string) { "abc def" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { 'abc' == subject.scan(/\w+/) }
+    And  { 'abc def' == string }
+  end
+
+  context 'when scan is passed an empty string' do
+    Given(:string) { "" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { nil == subject.scan(/\w+/) }
+  end
+end
