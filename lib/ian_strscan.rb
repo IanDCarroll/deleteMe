c8f7f1e2884292da51e9028cd855d7a9552d3173
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-19 16:10:24 -0700

    add eos? method to IanStrscan

--- lib/ian_strscan.rb
+++ lib/ian_strscan.rb
@@ -1,4 +1,11 @@
 class IanStrscan
+  attr_accessor :string
+
   def initialize(string)
+    @string = string
+  end
+
+  def eos?
+    @string.empty?
   end
 end
--- spec/ian_strscan_spec.rb
+++ spec/ian_strscan_spec.rb
@@ -5,3 +5,17 @@ describe 'IanStrscan initialize' do
   Given(:subject) { IanStrscan.new("abc def") }
   Then { raise_error != subject }
 end
+
+describe 'IanStrscan eos?' do
+  context 'when the string is not empty' do
+    Given(:string) { "abc def" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { false == subject.eos? }
+  end
+
+  context 'when the string is empty' do
+    Given(:string) { "" }
+    When(:subject) { IanStrscan.new(string) }
+    Then { true == subject.eos? }
+  end
+end
