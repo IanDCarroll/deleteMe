8d6ca43e90ea2c1396abe45c893133fcdf7eb378
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-22 11:11:10 -0700

    pass acceptance

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,1 +1,22 @@
+require 'rspec/given'
+require 'ian_strscan'
 
+describe 'Acceptance Test' do
+Given(:s) { IanStrscan.new('This is an example string') }
+Then { false     == s.eos?        }
+And  { "This"    == s.scan(/\w+/) }
+And  { nil       == s.scan(/\w+/) }
+And  { " "       == s.scan(/\s+/) }
+And  { nil       == s.scan(/\s+/) }
+And  { "is"      == s.scan(/\w+/) }
+And  { false     == s.eos?        }
+And  { " "       == s.scan(/\s+/) }
+And  { "an"      == s.scan(/\w+/) }
+And  { " "       == s.scan(/\s+/) }
+And  { "example" == s.scan(/\w+/) }
+And  { " "       == s.scan(/\s+/) }
+And  { "string"  == s.scan(/\w+/) }
+And  { true      == s.eos?        }
+And  { nil       == s.scan(/\s+/) }
+And  { nil       == s.scan(/\w+/) }
+end
