1b5be3258d3044b029e744db94865e646dc5b7b8
Ian Carroll <ian.d.carroll@gmail.com>
2017-05-24 10:08:00 -0700

    pass acceptance 4:51

--- lib/ian_strscan.rb
+++ lib/ian_strscan.rb
@@ -1,1 +1,20 @@
+class IanStrscan
+  attr_accessor :string
 
+  def initialize(string)
+    @string = string.dup
+  end
+
+  def eos?
+    @string.empty?
+  end
+
+  def scan(regex)
+    match = @string[regex]
+    if @string.start_with? match
+      @string.sub!(match, "")
+      match
+    end
+    rescue TypeError
+  end
+end
